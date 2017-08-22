33930be7da218775bdabe4cdbca858663918bcdb
Daniel Kanas <dkanas95@gmail.com>
2017-08-22 08:26:53 +0200

    mongoose configuration & connection

---
+++ server/db/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+
+mongoose.connect(URI)
