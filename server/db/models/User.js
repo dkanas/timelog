251e668f0621130edb80c6d11e5db87d60c34b64
Daniel Kanas <dkanas95@gmail.com>
2017-09-04 19:12:50 +0200

    updated the user model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  github_username: 'string',
-  role: 'string',
-  first_name: 'string',
-  last_name: 'string'
+  github_username: String,
+  role: { type: String, enum: ['employee', 'manager', 'admin'], default: 'employee' },
+  first_name: String,
+  last_name: String
 })
 
 const User = mongoose.model('User', userSchema)
