0d74b916b4cb6fe225ac07976c3c3389ddea09dc
Daniel Kanas <dkanas95@gmail.com>
2017-09-09 01:44:12 +0200

    debug mode in mongoose and exporting mongoose instance

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.set('debug', true)
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 mongoose.connect(URI)
+
+module.exports = mongoose
