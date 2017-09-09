ca9c8bc9076855a9102cd311be73b8fe5a5ba9d1
Daniel Kanas <dkanas95@gmail.com>
2017-09-09 02:33:46 +0200

    updated user model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('../index')
+const { crypto } = require('../../utils')
 
 const userSchema = new mongoose.Schema({
-  github_username: String,
+  githubId: Number,
+  githubToken: {
+    type: String,
+    set: crypto.encrypt,
+    get: crypto.decrypt
+  },
   role: { type: String, enum: ['employee', 'manager', 'admin'], default: 'employee' },
-  first_name: String,
-  last_name: String
+  firstName: String,
+  lastName: String
 })
 
 const User = mongoose.model('User', userSchema)
