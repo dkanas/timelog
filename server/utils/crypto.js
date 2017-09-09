04090a362caefeb46d2751229df13c0adb085b70
Daniel Kanas <dkanas95@gmail.com>
2017-09-09 02:32:43 +0200

    added default parameter value for crypto helpers

--- server/utils/crypto.js
+++ server/utils/crypto.js
@@ -2,12 +2,12 @@ const crypto = require('crypto')
 const alg = 'aes-256-ctr'
 const pwd = process.env.SESSION_SECRET
 
-const encrypt = text => {
+const encrypt = (text = '') => {
   const cipher = crypto.createCipher(alg, pwd)
   return cipher.update(text, 'utf8', 'hex') + cipher.final('hex')
 }
 
-const decrypt = text => {
+const decrypt = (text = '') => {
   const decipher = crypto.createDecipher(alg, pwd)
   return decipher.update(text, 'hex', 'utf8') + decipher.final('utf8')
 }
