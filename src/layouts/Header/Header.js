949cfa0b60e657bce07a86ba206f227f012cfef1
Daniel Kanas <dkanas95@gmail.com>
2017-09-19 22:14:50 +0200

    oauth redirection on button click

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -3,10 +3,11 @@ import React from 'react'
 import GithubLogo from './githublogo.png'
 import './Header.scss'
 
+const redirectToGithubOAuth = () => window.location.replace('/api/user/oauth')
 const Header = () => (
   <header className='header__container'>
     <h1>Timelog</h1>
-    <button className='header__login-btn'>
+    <button className='header__login-btn' onClick={redirectToGithubOAuth}>
       <label className='header__login-btn__label'>Sign in</label>
       <img src={GithubLogo} className='header__login-btn__github-logo'/>
     </button>
