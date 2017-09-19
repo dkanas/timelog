f4e535e7327626407587ff11584537d55e6c846a
Daniel Kanas <dkanas95@gmail.com>
2017-09-19 22:37:33 +0200

    getting safe fields from user model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,3 +1,4 @@
+const pick = require('lodash/pick')
 const mongoose = require('../index')
 const { crypto } = require('../../utils')
 
@@ -12,6 +13,12 @@ const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String
 })
+
+const safeFields = ['firstName', 'lastName', 'githubId', 'role']
+userSchema.methods.toSafeJSON = function () {
+  const unsafeJSON = this.toJSON()
+  return pick(unsafeJSON, safeFields)
+}
 
 const User = mongoose.model('User', userSchema)
 
