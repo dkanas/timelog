997083eb6dbf52c3f03eb196ffc0ee027a15a472
Daniel Kanas <dkanas95@gmail.com>
2017-09-26 22:43:10 +0200

    added a helper function to log errors

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
 export { default as api } from './api'
 
 export const to = promise => promise.then(res => ([null, res])).catch(e => [e])
+
+export const logError = error => process.env.NODE_ENV === 'development' && console.error(error)
