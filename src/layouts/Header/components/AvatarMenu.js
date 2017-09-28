941b047ae77a0f894e8ce524add28af3bd6d9624
Daniel Kanas <dkanas95@gmail.com>
2017-09-28 08:44:38 +0200

    refactored avatar menu

--- src/layouts/Header/components/AvatarMenu.js
+++ src/layouts/Header/components/AvatarMenu.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ClickOutside from 'react-click-outside'
+import cn from 'classnames'
 import './AvatarMenu.scss'
 
 const silenceClick = e => {
@@ -12,19 +14,18 @@ const MenuOption = ({ onClick, label }) => (
     onClick={onClick}>{label}</div>
 )
 const Menu = ({ isOpen, user, onClick }) => (
-  <div
-    className={'avatar-menu__dropdown__container ' + (isOpen ? 'open' : '')}>
-    <MenuOption onClick={silenceClick} label={user.githubLogin} />
-    <MenuOption onClick={onClick('logout')} label='Log out' />
-  </div>
+  <ClickOutside onClickOutside={onClick('clickOutside')}>
+    <div className={cn('avatar-menu__dropdown__container ', { open: isOpen })}>
+      <MenuOption onClick={silenceClick} label={user.githubLogin} />
+      <MenuOption onClick={onClick('logout')} label='Log out' />
+    </div>
+  </ClickOutside>
 )
 
 class AvatarMenu extends Component {
   state = { menuOpen: false }
-  onAvatarClick = () => {
-    const { menuOpen } = this.state
-    this.setState({ menuOpen: !menuOpen })
-  }
+  openMenu = () => this.setState({ menuOpen: true })
+  closeMenu = () => this.setState({ menuOpen: false })
   onLogoutClick = () => {
     this.props.logout()
     window.location.replace('/api/user/logout')
@@ -33,16 +34,18 @@ class AvatarMenu extends Component {
     switch (action) {
       case 'logout':
         return this.onLogoutClick()
+      case 'clickOutside':
+        return this.closeMenu()
     }
   }
   render () {
     const { user = {}, logout } = this.props
     const { menuOpen } = this.state
-    const triangleClassName = ['avatar-menu__triangle', menuOpen ? 'open' : ''].join(' ')
+
     return (
-      <div className='avatar-menu__container' onClick={this.onAvatarClick}>
+      <div className='avatar-menu__container' onClick={this.openMenu}>
         <img className='avatar-menu__img' src={user.avatar} />
-        <span className={triangleClassName}>&#9698;</span>
+        <span className={cn('avatar-menu__triangle', { open: menuOpen})}>&#9698;</span>
         <Menu
           isOpen={menuOpen}
           user={user}
